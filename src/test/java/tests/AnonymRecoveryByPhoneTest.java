506497699a7ce0cc8b276d768ef831d3a8459f70
Roman Tsapko <roma.tsapko.01@mail.ru>
2025-02-27 21:01:23 +0300

    Lesson 4

---
+++ src/test/java/tests/AnonymRecoveryByPhoneTest.java
@@ -0,0 +1,35 @@
+package tests;
+
+import core.base.BaseTest;
+import core.pages.AnonymRecoveryPage;
+import core.pages.LoginPage;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class AnonymRecoveryByPhoneTest extends BaseTest {
+
+    private static LoginPage loginPage;
+    private static AnonymRecoveryPage anonymRecoveryPage;
+
+    @BeforeEach
+    public void prepare() {
+        open(baseUrl);
+        loginPage = new LoginPage();
+    }
+
+    @Test
+    public void testAnonymRecoveryByPhone() {
+        // Попытка входа с некорректными данными
+        loginPage.login("incorrectUser", "incorrectPassword");
+
+        for (int i = 0; i < 2; i++) {
+            loginPage.loginWithOnlyPassword("incorrectPassword");
+            loginPage.clickLogin();
+        }
+
+        loginPage.goToRecovery();
+        anonymRecoveryPage = new AnonymRecoveryPage();
+    }
+}
